06e9441aaf371601cd46b0ba9238d0a039d0101c
njgarg22 <njgarg22@gmail.com>
2020-07-19 13:11:04 +0530

    Add PUT request method for message instance

--- src/main/java/com/neeraj/messenger/resources/MessageResource.java
+++ src/main/java/com/neeraj/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,6 +43,15 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
 	}
 	
 	@GET
